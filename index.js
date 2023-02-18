bd351476cc6e01d6312da1b1e21a5e8c617227a3
AlphaSender <125725022+AlphaSender@users.noreply.github.com>
2023-02-18 09:46:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-// Replace WEBHOOK_URL with your actual webhook URL
-const webhookUrl = 'https://discordapp.com/api/webhooks/1076406136179273879/LZLjYQOoj2lT_S_OQUI1Y4KcjlQ6b3z4NwKhSA_3SFSSZzj6uiouInQzE7bBeFQ6bjfK';
+// Discord webhook URL
+const webhookUrl = "https://discordapp.com/api/webhooks/1076406136179273879/LZLjYQOoj2lT_S_OQUI1Y4KcjlQ6b3z4NwKhSA_3SFSSZzj6uiouInQzE7bBeFQ6bjfK";
 
-// Define the message content as a JSON payload
-const payload = {
-  content: 'Hello, world!'
-};
+// Function to send a message to the webhook
+function sendMessage(message) {
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({content: message})
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to send message");
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
 
-// Send the message to the webhook
-fetch(webhookUrl, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(payload)
-})
-  .then(response => console.log(response))
-  .catch(error => console.error(error));
+// Example usage: send a message
+sendMessage("Hello, Discord!");
